4e900904e522f4fc87f368b3639a716bcd93fe3f
Phuong To <tophu95@gmail.com>
2019-05-10 20:52:25 +0100

    fetching json from URL instead of local json file

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,7 +18,8 @@ function getTitle() {
   //   .then(function (data) {
   //     console.log(data.reverse());
   //   });
-  fetch('../data.json')
+  // fetch('../data.json')
+  fetch('http://localhost:3000/api/todos')
     .then((res) => res.json())
     .then((data) => {
       let output = '<p>To do:</p>';
