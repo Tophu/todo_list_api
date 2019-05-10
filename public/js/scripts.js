299347a74178cfe078646e60480253fc8eefbed1
Phuong To <tophu95@gmail.com>
2019-05-10 20:25:08 +0100

    progress so far, need to sort json data in descending order

---
+++ public/js/scripts.js
@@ -0,0 +1,34 @@
+// let viewTasks = new Request("../data.json");
+
+// fetch(viewTasks)
+//   .then(function (task) {
+//     return task.json();
+//   })
+//   .then(function (data) {
+//     console.log(data.reverse());
+//   });
+
+document.getElementById('view-task').addEventListener('click', getTitle);
+
+function getTitle() {
+  // fetch('../data.json')
+  //   .then(function (res) {
+  //     return res.json();
+  //   })
+  //   .then(function (data) {
+  //     console.log(data.reverse());
+  //   });
+  fetch('../data.json')
+    .then((res) => res.json())
+    .then((data) => {
+      let output = '<p>To do:</p>';
+      data.forEach(function (task) {
+        output += `
+          <ul>
+            <li>Task ${task.id}: ${task.title}</li>
+          </ul>
+        `;
+      });
+      document.getElementById('display').innerHTML = output;
+    });
+}
