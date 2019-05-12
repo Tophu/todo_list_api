91ad087f2055334eb79ccb6546b65b9ea7746c26
Phuong To <tophu95@gmail.com>
2019-05-12 19:21:04 +0100

    styling completed

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,7 +8,7 @@ function getTitle() {
   fetch(jsonData)
     .then((res) => res.json())
     .then((data) => {
-      let output = '<p>All To Do:</p>';
+      let output = '<h2>All:</h2>';
       data.sort((a, b) => b - a).forEach(function (task) {
         // console.log(task.id);
         output += `
@@ -25,7 +25,7 @@ function completed() {
   fetch(jsonData)
     .then((res) => res.json())
     .then((data) => {
-      let output = `<p>Completed: ${data.length - 110}</p>`;
+      let output = `<h2>Completed: ${data.length - 110}</h2>`;
       data.forEach(function (task) {
         if (task.completed !== true) {
           return task
@@ -45,7 +45,7 @@ function toDo() {
   fetch(jsonData)
     .then((res) => res.json())
     .then((data) => {
-      let output = `<p>Completed: ${data.length - 90}</p>`;
+      let output = `<h2>To Do: ${data.length - 90}</h2>`;
       data.forEach(function (task) {
         if (task.completed === true) {
           return task
